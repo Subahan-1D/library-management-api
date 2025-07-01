cd0c76cc8396fb278f03f064bfbe7c4a8bbf3fcc
subahan ali <subahanislam523@gmail.com>
2025-07-01 23:46:13 +0600

    added to by book model or book schema

---
+++ src/app/models/book.model.ts
@@ -0,0 +1,57 @@
+import mongoose, { model, Schema } from "mongoose";
+import { IBook } from "../interfaces/book.interface";
+
+const bookSchema = new Schema<IBook>(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: [true, "Author is required"],
+      trim: true,
+    },
+    genre: {
+      type: String,
+      required: [true, "Genre is required"],
+      enum: [
+        "FICTION",
+        "NON_FICTION",
+        "SCIENCE",
+        "HISTORY",
+        "BIOGRAPHY",
+        "FANTASY",
+      ],
+      uppercase: true,
+    },
+    isbn: {
+      type: String,
+      required: [true, "ISBN is required"],
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+    copies: {
+      type: Number,
+      required: [true, "Copies is required"],
+      min: [0, "Copies must be a positive number"],
+      default: 1,
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+export const Book = model("Book", bookSchema);
