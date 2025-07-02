4500d5258c63eb67bf0c716d61985c81665dbef1
subahan ali <subahanislam523@gmail.com>
2025-07-02 11:46:33 +0600

    added to by a find book all data

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Application, Request, Response } from "express";
+import { bookRoutes } from "./app/controlers/book.controlers";
 
 const app: Application = express();
 
 app.use(express.json());
 
-app.get('/', (req : Request , res: Response)=>{
-    res.send(" Welcome Library Management App")
-})
+app.use("/api/books", bookRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(" Welcome Library Management App");
+});
 
 export default app;
---
+++ src/app/controlers/book.controlers.ts
@@ -0,0 +1,56 @@
+import express, { Request, Response } from "express";
+import { Book } from "../models/book.model";
+import { z } from "zod";
+import { handleError } from "../utils/errorHandaler";
+import { Query } from "mongoose";
+
+export const bookRoutes = express.Router();
+
+const BookSchema = z.object({
+  title: z.string().min(1, "Title is required"),
+  author: z.string().min(1, "Author is required"),
+  genre: z.string().min(1, "Genre is required"),
+  isbn: z.string().min(1, "ISBN is required"),
+  description: z.string().optional(),
+  copies: z.number().min(0, "Copies must be a positive number"),
+  available: z.boolean(),
+});
+
+bookRoutes.post("/", async (req: Request, res: Response) => {
+  try {
+    const booksParse = BookSchema.parse(req.body);
+    const book = await Book.create(booksParse);
+
+    res.status(201).json({
+      success: true,
+      message: "Book created successfully",
+      data: book,
+    });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      handleError(res, 400, "Validation failed", error);
+      return;
+    }
+
+    handleError(res, 500, "Failed to create book", error);
+  }
+});
+
+bookRoutes.get("/", async (req: Request, res: Response) => {
+  try {
+    const { filter, sortBy = "createdAt", sort = "asc", limit } = req.query;
+
+    const query: any = {};
+
+    if (filter) {
+      query.genre = filter.toString().toUpperCase();
+    }
+
+    const books = await Book.find({genre : "SCIENCE"});
+   res.status(200).json({
+      success: true,
+      message: "Books retrieved successfully",
+      data: books,
+    });
+  } catch (error) {}
+});
