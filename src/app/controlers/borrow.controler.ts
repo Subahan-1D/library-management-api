9687681a6ae11ce9b113a02b2a39fa9061442595
subahan ali <subahanislam523@gmail.com>
2025-07-03 00:09:16 +0600

    added to by aggregate pipeline or borrow model

--- src/app/controlers/borrow.controler.ts
+++ src/app/controlers/borrow.controler.ts
@@ -31,3 +31,43 @@ borrowRoutes.post("/", async (req: Request, res: Response) => {
   }
 });
 
+borrowRoutes.get("/", async (req: Request, res: Response) => {
+  try {
+    const summary = await Borrow.aggregate([
+      {
+        $group: {
+          _id: "$book",
+          totalQuantity: { $sum: "$quantity" },
+        },
+      },
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "_id",
+          as: "bookDetails",
+        },
+      },
+      {
+        $unwind: "$bookDetails",
+      },
+      {
+        $project: {
+          _id: 0,
+          book: {
+            title: "$bookDetails.title",
+            isbn: "$bookDetails.isbn",
+          },
+          totalQuantity: 1,
+        },
+      },
+    ]);
+    res.status(200).json({
+      success: true,
+      message: "Borrowed books summary retrieved successfully",
+      data: summary,
+    });
+  } catch (error) {
+    handleError(res, 500, "Failed to retrieve summary", error);
+  }
+});
